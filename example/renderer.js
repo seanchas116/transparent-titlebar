8307e30bd5cd9b2e130d00dee311cc5b32dedc0e
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-02 15:37:49 +0900

    Implement example

---
+++ example/renderer.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const Color = require('color')
+const {remote} = require('electron')
+const transparentTitlebar = remote.require('../')
+
+const setTitleButton = document.getElementById('setTitle')
+const titleInput = document.getElementById('title')
+const setRepresentedFilenameButton = document.getElementById('setRepresentedFilename')
+const colorInput = document.getElementById('color')
+
+let color = [0, 0, 0, 1]
+
+setTitleButton.addEventListener('click', () => {
+  const title = titleInput.value
+  if (title) {
+    remote.getCurrentWindow().setRepresentedFilename('')
+    remote.getCurrentWindow().setTitle(title)
+    transparentTitlebar.setTitleColor(remote.getCurrentWindow().getNativeWindowHandle(), ...color)
+  }
+})
+
+setRepresentedFilenameButton.addEventListener('click', () => {
+  remote.getCurrentWindow().setRepresentedFilename(__filename)
+  remote.getCurrentWindow().setTitle(path.basename(__filename))
+  transparentTitlebar.setTitleColor(remote.getCurrentWindow().getNativeWindowHandle(), ...color)
+})
+
+colorInput.addEventListener('change', () => {
+  const [r, g, b] = Color(colorInput.value).rgb().array()
+  color = [r/255, g/255, b/255, 1]
+  transparentTitlebar.setTitleColor(remote.getCurrentWindow().getNativeWindowHandle(), ...color)
+})
