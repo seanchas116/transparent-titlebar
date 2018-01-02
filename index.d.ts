475964eaf1572c5a194afdfc8ed035b1f8372b5f
Ryohei Ikegami <iofg2100@gmail.com>
2018-01-02 15:43:14 +0900

    Fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-export function setup(windowHandle: Buffer)
-export function setTitleColor(windowHandle: Buffer, r: number, g: number, b: number, a: number)
+export function setup(windowHandle: Buffer): void
+export function setTitleColor(windowHandle: Buffer, r: number, g: number, b: number, a: number): void
